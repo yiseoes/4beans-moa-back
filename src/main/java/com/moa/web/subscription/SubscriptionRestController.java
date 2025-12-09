83ce9020b164908e49e1a8d7c71d0fb395937c18
kjw <tj@DESKTOP-0QM92CP>
2025-12-09 18:11:28 +0900

    20251209 18:11 kjw SubscriptionRestController

--- src/main/java/com/moa/web/subscription/SubscriptionRestController.java
+++ src/main/java/com/moa/web/subscription/SubscriptionRestController.java
@@ -1,12 +1,22 @@
 package com.moa.web.subscription;
 
-import com.moa.dto.subscription.SubscriptionDTO;
-import com.moa.service.subscription.SubscriptionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moa.dto.subscription.SubscriptionDTO;
+import com.moa.service.subscription.SubscriptionService;
 
 @RestController
 @RequestMapping("/api/subscription")
@@ -17,10 +27,22 @@ public class SubscriptionRestController {
     @Autowired
     private SubscriptionService subscriptionService;
 
+    // @PostMapping
+    // public void addSubscription(@RequestBody SubscriptionDTO subscriptionDTO)
+    // throws Exception {
+    // logger.debug("Request [addSubscription] Time: {}, Content: {}",
+    // java.time.LocalDateTime.now(), subscriptionDTO);
+    // subscriptionService.addSubscription(subscriptionDTO);
+    // }
     @PostMapping
-    public void addSubscription(@RequestBody SubscriptionDTO subscriptionDTO) throws Exception {
+    public ResponseEntity<?> addSubscription(@RequestBody SubscriptionDTO subscriptionDTO) throws Exception {
         logger.debug("Request [addSubscription] Time: {}, Content: {}", java.time.LocalDateTime.now(), subscriptionDTO);
         subscriptionService.addSubscription(subscriptionDTO);
+
+        // 성공 응답 반환
+        return ResponseEntity.ok(Map.of(
+                "success", true,
+                "message", "구독이 성공적으로 신청되었습니다."));
     }
 
     @GetMapping("/{subscriptionId}")
@@ -31,9 +53,10 @@ public class SubscriptionRestController {
     }
 
     @GetMapping
-    public List<SubscriptionDTO> getSubscriptionList() throws Exception {
-        logger.debug("Request [getSubscriptionList] Time: {}", java.time.LocalDateTime.now());
-        return subscriptionService.getSubscriptionList();
+    public List<SubscriptionDTO> getSubscriptionList(
+            @org.springframework.web.bind.annotation.RequestParam String userId) throws Exception {
+        logger.debug("Request [getSubscriptionList] Time: {}, userId: {}", java.time.LocalDateTime.now(), userId);
+        return subscriptionService.getSubscriptionList(userId);
     }
 
     @PutMapping
