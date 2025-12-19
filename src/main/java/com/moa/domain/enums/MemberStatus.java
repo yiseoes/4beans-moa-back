9ffc9ba9cd07b006a58aac9fad15b92994a15fb3
Hansol <hansolp110@gmail.com>
2025-12-19 11:36:15 +0900

    Update: 파티 맴버 버그 수정. ENUM 상태값 추가

--- src/main/java/com/moa/domain/enums/MemberStatus.java
+++ src/main/java/com/moa/domain/enums/MemberStatus.java
@@ -1,7 +1,7 @@
 package com.moa.domain.enums;
 
 public enum MemberStatus {
-	PENDING_PAYMENT("결제대기"), ACTIVE("활성"), INACTIVE("비활성");
+	PENDING_PAYMENT("결제대기"), ACTIVE("활성"), INACTIVE("비활성"), LEFT("탈퇴");
 
 	private final String description;
 
